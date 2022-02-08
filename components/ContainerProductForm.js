56cdeca085f1c06c27b4daf21684c8669415dcc8
Carlos-vargs <cvargaslopez769@gmail.com>
2022-02-08 07:07:42 -0600

    add some changes

---
+++ components/ContainerProductForm.js
@@ -0,0 +1,163 @@
+import { useDisclosure, } from "@chakra-ui/react";
+import CategoryForm from "@components/CategoryForm";
+import ProductForm from "@components/ProductForm";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+function ContainerProductForm() {
+
+    const productsUrl = "https://pullandbear-clone-api.herokuapp.com/api/products"
+    const categoryUrl = "https://pullandbear-clone-api.herokuapp.com/api/products/categories"
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const [loading, setLoading] = useState(false)
+
+    const [categoryList, setCategoryList] = useState([])
+
+    const [productForm, setProductForm] = useState({
+        title: "",
+        size: "",
+        price: "",
+        gender: "",
+        mark: "",
+        category_id: "",
+        image: [],
+    })
+
+    const [categoryForm, setCategoryForm] = useState({ name: "" })
+
+
+    const handleSubmitProductForm = async e => {
+
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+
+        try {
+
+            let config = {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'multipart/form-data'
+                },
+            }
+
+            setLoading(true)
+
+            const response = await axios.post(productsUrl, formData, config)
+
+            if (response.status === 201) {
+
+                setLoading(false)
+
+                setProductForm({
+                    title: "",
+                    size: "",
+                    price: "",
+                    gender: "",
+                    mark: "",
+                    category_id: "",
+                    image: [],
+                })
+            }
+
+        } catch (error) {
+
+            setLoading(false)
+
+            console.error(error)
+
+        }
+
+    }
+
+    const handleChangeProductForm = e => {
+
+        const { name, value, files } = e.target
+
+        setProductForm({ ...productForm, [name]: value })
+
+        if (name === 'image') {
+            setProductForm({ ...productForm, image: [...files] })
+        }
+
+    }
+
+    function handleChangeCategoryForm(e) {
+
+        const { name, value } = e.target
+
+        setCategoryForm({ ...categoryForm, [name]: value })
+
+    }
+
+    const handleSubmitCategoryForm = async e => {
+
+        e.preventDefault()
+
+        const form = new FormData(e.target)
+
+        try {
+
+            setLoading(true)
+
+            const response = await axios.post(categoryUrl, form)
+
+            const { data: { data: newCategory } } = response
+
+            if (response.status === 201) {
+                setCategoryList([...categoryList, newCategory])
+                setProductForm({ ...productForm, category_id: newCategory.id })
+                setCategoryForm({ name: "" })
+                setLoading(false)
+                onClose()
+            }
+
+        } catch (error) {
+
+            setLoading(false)
+            console.error(error)
+        }
+
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    const getCategories = async () => {
+        try {
+            const { data: { data: categories } } = await axios.get(categoryUrl)
+
+            setCategoryList(categories)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+    return (
+        <>
+            <ProductForm
+                onSubmit={handleSubmitProductForm}
+                formValues={productForm}
+                onChange={handleChangeProductForm}
+                categoryList={categoryList}
+                onOpen={onOpen}
+                loading={loading}
+            />
+            <CategoryForm
+                isOpen={isOpen}
+                onClose={onClose}
+                onSubmit={handleSubmitCategoryForm}
+                onChange={handleChangeCategoryForm}
+                formValue={categoryForm}
+                loading={loading}
+            />
+        </>
+    );
+}
+
+export default ContainerProductForm;
--- pages/admin/create-products.js
+++ pages/admin/create-products.js
@@ -1,144 +1,10 @@
-import { Flex, useDisclosure, } from "@chakra-ui/react";
-import CategoryForm from "@components/CategoryForm";
-import ProductForm from "@components/ProductForm";
+import dynamic from "next/dynamic";
+import { Flex } from "@chakra-ui/react";
 import LogoCenter from "@components/LogoCenter";
-import { useState, useEffect } from "react";
-import axios from 'axios';
 
+const ContainerProductForm = dynamic(() => import("@components/ContainerProductForm"));
 
 function createProducts() {
-
-    const productsUrl = "https://pullandbear-clone-api.herokuapp.com/api/products"
-    const categoryUrl = "https://pullandbear-clone-api.herokuapp.com/api/products/categories"
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-    const [loading, setLoading] = useState(false)
-
-    const [categoryList, setCategoryList] = useState([])
-
-    const [productForm, setProductForm] = useState({
-        title: "",
-        size: "",
-        price: "",
-        gender: "",
-        mark: "",
-        category_id: "",
-        image: [],
-    })
-
-    const [categoryForm, setCategoryForm] = useState({ name: "" })
-
-
-    const handleSubmitProductForm = async e => {
-
-        e.preventDefault()
-
-        const formData = new FormData(e.target)
-
-        try {
-
-            let config = {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'multipart/form-data'
-                },
-            }
-
-            setLoading(true)
-
-            const response = await axios.post(productsUrl, formData, config)
-
-            if (response.status === 201) {
-
-                setLoading(false)
-
-                setProductForm({
-                    title: "",
-                    size: "",
-                    price: "",
-                    gender: "",
-                    mark: "",
-                    category_id: "",
-                    image: [],
-                })
-            }
-
-        } catch (error) {
-
-            setLoading(false)
-
-            console.error(error)
-
-        }
-
-    }
-
-    const handleChangeProductForm = e => {
-
-        const { name, value, files } = e.target
-
-        setProductForm({ ...productForm, [name]: value })
-
-        if (name === 'image') {
-            setProductForm({ ...productForm, image: [...files] })
-        }
-
-    }
-
-    function handleChangeCategoryForm(e) {
-
-        const { name, value } = e.target
-
-        setCategoryForm({ ...categoryForm, [name]: value })
-
-    }
-
-    const handleSubmitCategoryForm = async e => {
-
-        e.preventDefault()
-
-        const form = new FormData(e.target)
-
-        try {
-
-            setLoading(true)
-
-            const response = await axios.post(categoryUrl, form)
-
-            const { data: { data: newCategory } } = response
-
-            if (response.status === 201) {
-                setCategoryList([...categoryList, newCategory])
-                setProductForm({ ...productForm, category_id: newCategory.id })
-                setCategoryForm({ name: "" })
-                setLoading(false)
-                onClose()
-            }
-
-        } catch (error) {
-
-            setLoading(false)
-            console.error(error)
-        }
-
-    }
-
-    useEffect(() => {
-        getCategories()
-    }, [])
-
-    const getCategories = async () => {
-        try {
-            const { data: { data: categories } } = await axios.get(categoryUrl)
-
-            setCategoryList(categories)
-
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
 
     return (
         <Flex
@@ -150,22 +16,7 @@ function createProducts() {
             px={['0px', '0px', '100px', '100px', '100px']}
         >
             <LogoCenter w={['200px', '250px', '250px', '250px', '250px']} />
-            <ProductForm
-                onSubmit={handleSubmitProductForm}
-                formValues={productForm}
-                onChange={handleChangeProductForm}
-                categoryList={categoryList}
-                onOpen={onOpen}
-                loading={loading}
-            />
-            <CategoryForm
-                isOpen={isOpen}
-                onClose={onClose}
-                onSubmit={handleSubmitCategoryForm}
-                onChange={handleChangeCategoryForm}
-                formValue={categoryForm}
-                loading={loading}
-            />
+            <ContainerProductForm />
         </Flex>
     );
 }
